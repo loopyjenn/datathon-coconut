178473373780c77b04490403d528ebd28e4be4d3
loopyjenn <lpeng3@uw.edu>
2024-05-19 07:31:23 -0700

    Updates

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export function Footer(props) {
-    return (
-        <footer>
-          &copy; 2024 Health: Drug Overdose in USA
-        </footer>
-    );
-}
--- src/components/Header.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export function Header (props) {
-    return (
-        <header className="App-header">
-            <h1>Health: Drug Overdose in USA</h1>
-      </header>
-    );
-}
